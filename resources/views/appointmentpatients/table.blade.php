5d8e91aa41fb0d6c84eb83af92f0fe8d9cc228e8
allyasyazana <allyaasyazana99@gmail.com>
2022-01-06 02:21:44 +0800

    commit

---
+++ resources/views/appointmentpatients/table.blade.php
@@ -0,0 +1,56 @@
+@section('content')
+<div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>List of Appointment</h2>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <html>
+        <style>
+            th {
+                padding-top: 12px;
+                padding-bottom: 12px;
+                text-align: left;
+                background-color: #0A7825;
+                color: white;
+                }
+        </style>
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Patient Name</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($appointments as $s)
+        <tr>
+            <td>{{ $s->id }}</td>
+            <td>{{ $s->patient_name }}</td>
+            <td>{{ $s->date}}</td>
+            <td>{{ $s->time}}</td>
+            
+            <td>
+                <form action="{{ route('appointments.destroy',$s->id) }}" method="POST">
+    
+                    <a class="btn btn-primary" href="{{ route('appointments.edit',$s->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+@endsection
